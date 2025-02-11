ae3bfc6bb988cd85d36867cd8a67b186239f870f
SebiBoica <sebastian@graffino.com>
2025-02-11 14:59:32 +0200

    feat: added new components

---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Task details</div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <RouterLink to="">
+    <li class="item">{{ item.id }} {{ item.title }} {{ item.completed }}</li>
+  </RouterLink>
+</template>
+
+<script setup lang="ts">
+  import { defineProps } from 'vue';
+
+  const props = defineProps(['item'])
+</script>
+
+<style lang="scss" scoped>
+  .item {
+    list-style-type: none;
+    font-weight: bold;
+  }
+</style>
