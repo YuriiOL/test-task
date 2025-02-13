d21354aacbee41bd4aadacd6981ea3f3543244db
Daniel Krikava <daniel.krikava@gmail.com>
2025-02-13 13:39:45 +0100

    feat: Technical Interview Todo list implementation

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="task">
+		<p>Task ID: {{ props.taskId }}</p>
+		<router-link :to="{ name: 'TaskDetails', params: { id: props.taskId } }">
+			>Title: {{ props.taskTitle }}</router-link
+		>
+		<p>Completed: {{ props.taskCompleted }}</p>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface Props {
+	taskId: number;
+	taskTitle: string;
+	taskCompleted: boolean;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style>
+.task {
+	border: 1px solid black;
+	border-radius: 20px;
+	max-width: 200px;
+	padding: 0 20px;
+}
+</style>
--- src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="home-wrapper">
-      <h1>Home Page</h1>
-      <p>Welcome to the home page!</p>
-    </div>
-</template>
-
-<script setup lang="ts">
-</script>
